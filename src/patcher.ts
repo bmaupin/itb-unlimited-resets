43e2d4558e99dcff9d38a96f8937adfcc89b1342
bmaupin <bmaupin@users.noreply.github.com>
2025-08-13 13:22:06 -0400

    feat: Integrate Linux patch functionality into patcher
    - Add logic for Linux patch and separate from Windows logic
    - Reorganise functions, keeping higher-level functions near the top
    - Update toLittleEndianBytes to support negative numbers

--- src/patcher.ts
+++ src/patcher.ts
@@ -1,3 +1,4 @@
+import { open } from 'elfinfo';
 import { promises as fs } from 'fs';
 import * as PE from 'pe-library';
 
@@ -10,17 +11,22 @@ if (!debug) {
   console.debug = () => {};
 }
 
+const UNSUPPORTED_BINARY = 'Error: File is neither a Linux nor Windows binary';
+
 const main = async () => {
   const fileToPatch = getArguments();
   const fileData = await fs.readFile(fileToPatch);
 
-  if (isWindowsBinary(fileData)) {
-    console.log('Detected Windows binary format');
-    patchWindowsFileData(fileData);
-  } else if (isLinuxBinary(fileData)) {
+  if (isLinuxBinary(fileData)) {
     console.log('Detected Linux binary format');
-    throw new Error('This functionality has not yet been implemented');
+  } else if (isWindowsBinary(fileData)) {
+    console.log('Detected Windows binary format');
+  } else {
+    throw new Error(UNSUPPORTED_BINARY);
   }
+
+  giveUnlimitedTurnResets(fileData);
+  await disableConfirmationPopup(fileData);
 
   if (!debug) {
     await fs.writeFile(fileToPatch, fileData);
@@ -53,68 +59,47 @@ const isWindowsBinary = (fileData: Buffer): boolean => {
   return fileData.length > 2 && fileData[0] === 0x4d && fileData[1] === 0x5a;
 };
 
-const patchWindowsFileData = (fileData: Buffer) => {
-  giveUnlimitedTurnResets(fileData);
-  disableConfirmationPopup(fileData);
-};
-
 const giveUnlimitedTurnResets = (fileData: Buffer) => {
   console.log('Enabling unlimited turn resets');
-  // Replace DEC with NOP to ensure the reset turn counter never goes down
-  //  589a98:       8b 91 5c 45 00 00       mov    0x455c(%ecx),%edx
-  //  589a9e:       8d 45 f0                lea    -0x10(%ebp),%eax
-  //  589aa1:       4a                      dec    %edx
-  const searchBytes = Buffer.from([
-    0x8b, 0x91, 0x5c, 0x45, 0x00, 0x00, 0x8d, 0x45, 0xf0, 0x4a,
-  ]);
+
+  let searchBytes: Buffer;
+  let newBytes: Buffer;
+
+  if (isLinuxBinary(fileData)) {
+    // Decrement 0 instead of 1 to ensure the reset turn counter never goes down
+    //  8a58c4:       83 af 1c 45 00 00 01    subl   $0x1,0x451c(%rdi)
+    searchBytes = Buffer.from([0x83, 0xaf, 0x1c, 0x45, 0x00, 0x00, 0x01]);
+    newBytes = Buffer.from([0x83, 0xaf, 0x1c, 0x45, 0x00, 0x00, 0x00]);
+  } else if (isWindowsBinary(fileData)) {
+    // Replace DEC with NOP to ensure the reset turn counter never goes down
+    //  589a98:       8b 91 5c 45 00 00       mov    0x455c(%ecx),%edx
+    //  589a9e:       8d 45 f0                lea    -0x10(%ebp),%eax
+    //  589aa1:       4a                      dec    %edx
+    searchBytes = Buffer.from([
+      0x8b, 0x91, 0x5c, 0x45, 0x00, 0x00, 0x8d, 0x45, 0xf0, 0x4a,
+    ]);
+    newBytes = Buffer.from([
+      0x8b, 0x91, 0x5c, 0x45, 0x00, 0x00, 0x8d, 0x45, 0xf0, 0x90,
+    ]);
+  } else {
+    throw new Error(UNSUPPORTED_BINARY);
+  }
+
   const offset = findBytes(fileData, searchBytes);
-  patchOffset(
-    fileData,
-    offset,
-    Buffer.from([0x8b, 0x91, 0x5c, 0x45, 0x00, 0x00, 0x8d, 0x45, 0xf0, 0x90])
-  );
+  patchOffset(fileData, offset, newBytes);
 };
 
-const findBytes = (fileData: Buffer, searchBytes: Buffer): number => {
-  // Find the offset of the search bytes in the file data
-  const offset = fileData.indexOf(searchBytes);
-  if (offset === -1) {
-    console.error('Bytes not found in file:', searchBytes);
-    throw new Error(
-      'Unable to find address to patch; has the file already been patched?'
-    );
-  }
-  return offset;
-};
-
-const patchOffset = (fileData: Buffer, offset: number, patchBytes: Buffer) => {
-  // Check if the offset is within the bounds of the file data
-  if (offset < 0 || offset + patchBytes.length > fileData.length) {
-    throw new Error('Offset is out of bounds for the file data');
-  }
-
-  const originalBytes = fileData.slice(offset, offset + patchBytes.length);
-  console.log(
-    `Patching bytes at offset ${offset} from ${originalBytes.toString(
-      'hex'
-    )} to ${patchBytes.toString('hex')}`
-  );
-
-  // Replace the bytes at the specified offset with the patch bytes
-  fileData.fill(patchBytes, offset, offset + patchBytes.length);
-};
-
-const disableConfirmationPopup = (fileData: Buffer) => {
-  const undoTurnOffset = findUndoTurnOffset(fileData);
+const disableConfirmationPopup = async (fileData: Buffer) => {
+  const undoTurnOffset = await findUndoTurnOffset(fileData);
   const flagOffset = dontBlockOnConfirmation(fileData, undoTurnOffset);
   dontShowConfirmationPopup(fileData, undoTurnOffset, flagOffset);
 };
 
-const findUndoTurnOffset = (fileData: Buffer) => {
+const findUndoTurnOffset = async (fileData: Buffer) => {
   const stringFileOffset = fileData.indexOf('Reset_TurnFinal');
   console.debug(`Found string 'Reset_Turn' at offset: ${stringFileOffset}`);
 
-  const stringMemoryAddress = fileOffsetToVirtualAddress(
+  const stringMemoryAddress = await fileOffsetToVirtualAddress(
     fileData,
     stringFileOffset
   );
@@ -135,11 +120,23 @@ const findUndoTurnOffset = (fileData: Buffer) => {
     )}`
   );
 
-  // Starting from firstUsageAddress, search backwards for the start of the function:
-  //  591100:       55                      push   %ebp
-  //  591101:       8b ec                   mov    %esp,%ebp
-  //  591103:       6a ff                   push   $0xffffffff
-  const functionStartBytes = Buffer.from([0x55, 0x8b, 0xec, 0x6a, 0xff]);
+  let functionStartBytes: Buffer;
+
+  if (isLinuxBinary(fileData)) {
+    // Starting from firstUsageAddress, search backwards for the start of the function:
+    //  8a7200:       53                      push   %rbx
+    //  8a7201:       48 89 fb                mov    %rdi,%rbx
+    functionStartBytes = Buffer.from([0x53, 0x48, 0x89, 0xfb]);
+  } else if (isWindowsBinary(fileData)) {
+    // Starting from firstUsageAddress, search backwards for the start of the function:
+    //  591100:       55                      push   %ebp
+    //  591101:       8b ec                   mov    %esp,%ebp
+    //  591103:       6a ff                   push   $0xffffffff
+    functionStartBytes = Buffer.from([0x55, 0x8b, 0xec, 0x6a, 0xff]);
+  } else {
+    throw new Error(UNSUPPORTED_BINARY);
+  }
+
   let undoTurnOffset = -1;
 
   for (let i = firstUsageAddress; i >= 0; i--) {
@@ -162,46 +159,42 @@ const findUndoTurnOffset = (fileData: Buffer) => {
   }
 
   return undoTurnOffset;
-
-  // // Starting from that address, find the offset of the bytes to patch
-  // const searchBuffer = new Uint8Array([0x85, 0xc0, 0x74]);
-  // const searchOffset = fileData.indexOf(searchBuffer, firstUsageAddress);
-
-  // if (searchOffset === -1 || searchOffset - firstUsageAddress > 512) {
-  //   console.log('Unable to apply patch; has the file already been patched?');
-  //   process.exit();
-  // }
-};
-
-const fileOffsetToVirtualAddress = (fileData: Buffer, fileOffset: number) => {
-  const exe = PE.NtExecutable.from(fileData, { ignoreCert: true });
-  const rdataInfo = exe
-    .getAllSections()
-    .filter((section) => section.info.name === '.rdata')[0].info;
-  const rdataFileOffset = rdataInfo.pointerToRawData;
-  const rdataMemoryAddress = rdataInfo.virtualAddress + exe.getImageBase();
-  const virtualAddress = fileOffset - rdataFileOffset + rdataMemoryAddress;
-
-  return virtualAddress;
 };
 
 const dontBlockOnConfirmation = (fileData: Buffer, undoTurnOffset: number) => {
   console.log('Bypassing confirmation dialogue response check');
-  // Set the flag at 0x4560 instead of 0x4558 to skip waiting for the confirmation dialogue response
-  //  591143:       c7 86 58 45 00 00 01    movl   $0x1,0x4558(%esi)
-  //  59114a:       00 00 00
-  const searchBytes = Buffer.from([
-    0xc7, 0x86, 0x58, 0x45, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
-  ]);
+
+  let searchBytes: Buffer;
+  let newBytes: Buffer;
+
+  if (isLinuxBinary(fileData)) {
+    // Set the flag at 0x4520 instead of 0x4518 to skip waiting for the confirmation dialogue response
+    //  8a722a:       c7 83 18 45 00 00 01    movl   $0x1,0x4518(%rbx)
+    //  8a7231:       00 00 00
+    searchBytes = Buffer.from([
+      0xc7, 0x83, 0x18, 0x45, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+    ]);
+    newBytes = Buffer.from([
+      0xc7, 0x83, 0x20, 0x45, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+    ]);
+  } else if (isWindowsBinary(fileData)) {
+    // Set the flag at 0x4560 instead of 0x4558
+    //  591143:       c7 86 58 45 00 00 01    movl   $0x1,0x4558(%esi)
+    //  59114a:       00 00 00
+    searchBytes = Buffer.from([
+      0xc7, 0x86, 0x58, 0x45, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+    ]);
+    newBytes = Buffer.from([
+      0xc7, 0x86, 0x60, 0x45, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00,
+    ]);
+  } else {
+    throw new Error(UNSUPPORTED_BINARY);
+  }
+
   const flagOffset =
     findBytes(fileData.slice(undoTurnOffset), searchBytes) + undoTurnOffset;
   console.debug('flagOffset=', flagOffset);
-
-  patchOffset(
-    fileData,
-    flagOffset,
-    Buffer.from([0xc7, 0x86, 0x60, 0x45, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00])
-  );
+  patchOffset(fileData, flagOffset, newBytes);
 
   return flagOffset;
 };
@@ -214,8 +207,56 @@ const dontShowConfirmationPopup = (
   console.log(
     "Don't show the confirmation dialogue to prevent it from flashing"
   );
-  clearUndoTurnPushInstructions(fileData, flagOffset);
-  skipConfirmationPopupCreation(fileData, flagOffset);
+
+  let returnBytes: Buffer;
+
+  if (isLinuxBinary(fileData)) {
+    // Find the offset to jump to
+    //  8a7211:       48 83 c4 30             add    $0x30,%rsp
+    //  8a7215:       5b                      pop    %rbx
+    //  8a7216:       c3                      ret
+    returnBytes = Buffer.from([0x48, 0x83, 0xc4, 0x30]);
+  } else if (isWindowsBinary(fileData)) {
+    // Find the offset to jump to
+    // 5911f4:       8b 4d f4                mov    -0xc(%ebp),%ecx
+    returnBytes = Buffer.from([0x8b, 0x4d, 0xf4]);
+  } else {
+    throw new Error(UNSUPPORTED_BINARY);
+  }
+
+  // Length of the instruction that sets the flag
+  const flagInstructionLength = 10;
+  const jumpOffset = flagOffset + flagInstructionLength;
+  const jumpInstructionLength = 5;
+  const returnOffset =
+    findBytes(
+      fileData.slice(undoTurnOffset + jumpInstructionLength),
+      returnBytes
+    ) + undoTurnOffset;
+  console.debug('returnOffset=', returnOffset);
+
+  const returnRelativeOffset = returnOffset - jumpOffset;
+  console.debug('returnRelativeOffset=', returnRelativeOffset);
+
+  // Patch the JMP instruction to skip the confirmation dialogue
+  // Linux:
+  //  8a7234:       e9 d8 ff ff ff          jmp    8a7211
+  // Windows:
+  //  59114d:       e9 a2 00 00 00          jmp    0x5911f4
+  const jumpPatchBytes = Buffer.from([
+    0xe9, // JUMP
+    ...toLittleEndianBytes(returnRelativeOffset),
+  ]);
+  console.debug('jumpPatchBytes=', jumpPatchBytes);
+  patchOffset(fileData, jumpOffset, jumpPatchBytes);
+
+  if (isWindowsBinary(fileData)) {
+    // On Windows, we're writing a 5-byte instruction over a 7-byte instruction, so NOP the final bytes
+    const nopBytes = Buffer.from([0x90, 0x90]);
+    patchOffset(fileData, jumpOffset + 5, nopBytes);
+
+    clearUndoTurnPushInstructions(fileData, flagOffset);
+  }
 };
 
 const clearUndoTurnPushInstructions = (
@@ -263,38 +304,69 @@ const clearUndoTurnPushInstructions = (
   patchOffset(fileData, leaOffset - 7, nopBytes);
 };
 
-const skipConfirmationPopupCreation = (
-  fileData: Buffer,
-  flagOffset: number
-) => {
-  // Find the offset to jump to
-  // 5911f4:       8b 4d f4                mov    -0xc(%ebp),%ecx
-  const returnBytes = Buffer.from([0x8b, 0x4d, 0xf4]);
-  const flagInstructionLength = 10;
-  const jumpOffset = flagOffset + flagInstructionLength;
-  const jumpInstructionLength = 5;
-  const returnRelativeOffset = findBytes(
-    fileData.slice(jumpOffset + jumpInstructionLength),
-    returnBytes
-  );
-  console.debug('returnRelativeOffset=', returnRelativeOffset);
-
-  // Patch the JMP instruction to skip the confirmation dialogue
-  //  59114d:       e9 a2 00 00 00          jmp    0x5911f4
-  const jumpPatchBytes = Buffer.from([
-    0xe9, // JUMP
-    ...toLittleEndianBytes(returnRelativeOffset),
-    // We're writing a 5-byte instruction over a 7-byte instruction, so NOP the final bytes
-    0x90,
-    0x90,
-  ]);
-  console.debug('jumpPatchBytes=', jumpPatchBytes);
-  patchOffset(fileData, jumpOffset, jumpPatchBytes);
+const findBytes = (fileData: Buffer, searchBytes: Buffer): number => {
+  // Find the offset of the search bytes in the file data
+  const offset = fileData.indexOf(searchBytes);
+  if (offset === -1) {
+    console.error('Bytes not found in file:', searchBytes);
+    throw new Error(
+      'Unable to find address to patch; has the file already been patched?'
+    );
+  }
+  return offset;
 };
 
-const toLittleEndianBytes = (num: number): Buffer => {
+const patchOffset = (fileData: Buffer, offset: number, patchBytes: Buffer) => {
+  // Check if the offset is within the bounds of the file data
+  if (offset < 0 || offset + patchBytes.length > fileData.length) {
+    throw new Error('Offset is out of bounds for the file data');
+  }
+
+  const originalBytes = fileData.slice(offset, offset + patchBytes.length);
+  console.log(
+    `    Patching bytes at offset ${offset} from ${originalBytes.toString(
+      'hex'
+    )} to ${patchBytes.toString('hex')}`
+  );
+
+  // Replace the bytes at the specified offset with the patch bytes
+  fileData.fill(patchBytes, offset, offset + patchBytes.length);
+};
+
+const fileOffsetToVirtualAddress = async (
+  fileData: Buffer,
+  fileOffset: number
+) => {
+  let virtualAddress: number;
+  if (isLinuxBinary(fileData)) {
+    const info = await open(fileData);
+    const textSection = info.elf?.sections.find((s) => s.name === '.text');
+    if (!textSection) {
+      throw new Error('Error: Unable to find .text section in ELF binary');
+    }
+
+    const textFileOffset = textSection.offset;
+    const textVirtualAddress = Number(textSection.addr);
+    virtualAddress = fileOffset - textFileOffset + textVirtualAddress;
+  } else if (isWindowsBinary(fileData)) {
+    const exe = PE.NtExecutable.from(fileData, { ignoreCert: true });
+    const rdataInfo = exe
+      .getAllSections()
+      .filter((section) => section.info.name === '.rdata')[0].info;
+
+    const rdataFileOffset = rdataInfo.pointerToRawData;
+    const rdataMemoryAddress = rdataInfo.virtualAddress + exe.getImageBase();
+    virtualAddress = fileOffset - rdataFileOffset + rdataMemoryAddress;
+  } else {
+    throw new Error(UNSUPPORTED_BINARY);
+  }
+
+  return virtualAddress;
+};
+
+const toLittleEndianBytes = (num) => {
   const buf = Buffer.alloc(4);
-  buf.writeUInt32LE(num);
+  buf.writeInt32LE(num);
   return buf;
 };
 
